f12eb9bb566c0d595fbba5a96c7c2b085f4d7f0e
stevenkhwun <stevenkhwun@gmail.com>
2022-07-24 12:11:58 +0800

    Files modified.

--- Rscripts/LoadData.R
+++ Rscripts/LoadData.R
@@ -5,13 +5,18 @@
 # Packages `foreign` required
 library(foreign)               # Load the required package
 
-# Input the Stat data file `wage.dta` 
+# Input the Stata data file `wage.dta` 
 # `wage.dta` is a data file accompany the book "An Introduction to Classical Econometric Theory (2000)" by
 # Paul A. Ruud
 
 DTAdata <- read.dta("./Data/wage.dta")
 head(DTAdata)
 
+# Further example of importing Stata data file from internet
+affairs <- read.dta("http://fmwww.bc.edu/ec-p/data/wooldridge/affairs.dta")
+head(affairs)
+
+##########################################################################################################
 # Importing SPSS data file into R
 # Packages `haven` required
 library(haven)               # Load the required package
@@ -23,9 +28,11 @@ library(haven)               # Load the required package
 SPSSdata <- read_sav("./Data/Cats and Dogs.sav")
 head(SPSSdata)
 
+##########################################################################################################
 # Display the objects created when importing the data files
 ls()
 
 # Removing the objects
 rm(DTAdata)
 rm(SPSSdata)
+rm(affairs)
